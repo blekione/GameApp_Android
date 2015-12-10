93c383d1437c01cb5986f1351930ee4efd98333e
Marcin Kruglik <marcin.kruglik@gmail.com>
2015-12-10 16:11:46 +0000

    ADD BUTTON TO CHOSE PLAYER FROM THE LIST
    and test text field to see if onClick action is working

--- app/src/main/java/com/blekione/wordgame/StartActivity.java
+++ app/src/main/java/com/blekione/wordgame/StartActivity.java
@@ -1,7 +1,10 @@
 package com.blekione.wordgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+
+    public void onClickAddPlayer(View view) {
+        Intent intent = new Intent(this, AddNewPlayer.class);
+        startActivity(intent);
+        TextView welcomeMsg = (TextView) findViewById(R.id.welcome_message);
+        welcomeMsg.setText("Welcome! Bleki");
+    }
+
 }
