8f59fea4a3a18b091fa4c5bb672209af4ab9cc36
Marcin Kruglik <marcin.kruglik@gmail.com>
2015-12-13 11:32:45 +0000

    ADD POPUP WINDOW WITH RESULT OF GUESS AND PASSWORD
    Add password as random word from list of words.
    Implement check if guess word match password and display message
    according to result

--- app/src/main/java/com/blekione/wordgame/GameActivity.java
+++ app/src/main/java/com/blekione/wordgame/GameActivity.java
@@ -1,16 +1,23 @@
 package com.blekione.wordgame;
 
-import android.graphics.Color;
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridLayout.*;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 import java.util.List;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -20,11 +27,18 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         GridLayout gridLayout = (GridLayout) findViewById(R.id.buttons_layout);
 
+        // create reference to game difficulty level and list of games words
         int difficultyLvl = DifficultyLevelActivity.getDifficultyLvl();
-        List<String> gameWords = DifficultyLevelActivity.getGameWords();
-        Button btns[] = new Button[difficultyLvl*2 + 3];
-        for (int i = 0; i < btns.length; i++) {
-            final int index = i;
+        final List<String> gameWords = DifficultyLevelActivity.getGameWords();
+
+        // randomly chose one word from list to be password
+        Random random = new Random();
+        final String password = gameWords.get(random.nextInt(gameWords.size()));
+
+
+        // create layout dynamical based on difficulty level
+        for (int i = 0; i < gameWords.size(); i++) {
+            final int index = i; // to be used in OnClickListener anonymous classes
             // display words from list of gameWords
             TextView textView = new TextView(this);
             textView.setText(gameWords.get(i));
@@ -40,11 +54,15 @@ public class GameActivity extends AppCompatActivity {
             paramBtnCheck.columnSpec = GridLayout.spec(1);
             paramBtnCheck.rowSpec = GridLayout.spec(i);
             gridLayout.addView(btnCheck, paramBtnCheck);
-            btnCheck.setOnClickListener(new View.OnClickListener() {
-
+            btnCheck.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                        
+                    if (gameWords.get(index).equals(password)) {
+                        showPopupWindow(GameActivity.this, "You guess correctly");
+                    } else {
+                        String msg = numberOfCorrectLetters(gameWords.get(index), password);
+                        showPopupWindow(GameActivity.this, msg);
+                    }
                 }
             });
             // display buttons to cross the word
@@ -54,7 +72,7 @@ public class GameActivity extends AppCompatActivity {
             paramBtnCross.columnSpec = GridLayout.spec(2);
             paramBtnCross.rowSpec = GridLayout.spec(i);
             gridLayout.addView(btnCross, paramBtnCross);
-            btnCross.setOnClickListener(new View.OnClickListener() {
+            btnCross.setOnClickListener(new OnClickListener() {
 
                 @Override
                 public void onClick(View view) {
@@ -66,6 +84,56 @@ public class GameActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void showPopupWindow(final Activity context, String msg) {
+
+        // inflate the popup_layout.xml
+        LinearLayout viewGroup = (LinearLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.popup_layout, viewGroup);
+
+        // create the popup window
+        int popupWidth = 500;
+        int popupHeight = 450;
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(popupWidth);
+        popup.setHeight(popupHeight);
+        popup.setFocusable(true);
+
+
+        // display the popup at the center of the screen
+        popup.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+        // get a reference to element in popup layout
+        TextView testText = (TextView) layout.findViewById(R.id.test_popup);
+        testText.setText(msg);
+
+        // get a reference to Close button
+        Button closeBtn = (Button) layout.findViewById(R.id.close);
+        closeBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                popup.dismiss(); // close popup window when close button pressed
+            }
+        });
+    }
+
+    /**
+     * return how many letters in input words are on same positions
+     * @param guessWord
+     * @param passwordWord
+     * @return
+     */
+    public static String numberOfCorrectLetters(String guessWord, String passwordWord) {
+        int correctLetters = 0;
+        for (int i = 0; i < guessWord.length(); i++)
+            if (guessWord.charAt(i) == passwordWord.charAt(i))
+                correctLetters++;
+        return correctLetters + "/" + guessWord.length() + " correct" + "\n";
     }
 
 }
